e4955caca42aa76d98f6979a5acbcc32710e1e51
unknown <raulsouth@gmail.com>
2023-01-13 13:23:45 -0300

    agrago carpeta con imagenes en asset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArProgramaComponent } from './components/logo-ar-programa/logo-ar-programa.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArProgramaComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
