1ef1d99e196ea27ab3b2d5d90dd6e4e697d31140
unknown <raulsouth@gmail.com>
2023-01-21 00:47:36 -0300

    más comoponentes en angular y cambios en app-header

--- Angular-proyecto/src/app/app.module.ts
+++ Angular-proyecto/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArProgramaComponent } from './components/logo-ar-programa/logo-ar-programa.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
+import { RedSocialComponent } from './components/red-social/red-social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoArProgramaComponent,
-    AcercadeComponent
+    AcercadeComponent,
+    RedSocialComponent
   ],
   imports: [
     BrowserModule,
