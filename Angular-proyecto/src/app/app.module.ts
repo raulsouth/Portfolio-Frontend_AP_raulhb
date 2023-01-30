8c4afebb884efb87b608c4056939bf2621c37dc7
unknown <raulsouth@gmail.com>
2023-01-30 20:19:29 -0300

    Angular: seccion Hard y Soft Skills

--- Angular-proyecto/src/app/app.module.ts
+++ Angular-proyecto/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { RedSocialComponent } from './components/red-social/red-social.component
 import { BannerComponent } from './components/banner/banner.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+// Import ng-circle-progress
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardsoftComponent } from './components/hardsoft/hardsoft.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +23,13 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     RedSocialComponent,
     BannerComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HardsoftComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
